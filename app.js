2b3f0c981841894269250ad80f9643cc6c6218b1
Sebastian Budyński <Sebbudynski@gmail.com>
2024-01-03 18:54:52 +0100

    cleaning the code, aded minus icon

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 "use strict";
 
-
 const acc = document.getElementsByClassName("question");
 
 let i;
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function () {
+    /* Toggle between adding and removing the "active" class,
+    to change icon  'plus' to 'minus' */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
     let hidden = this.nextElementSibling;
-    if (hidden.style.display === "block") {
+    if (hidden.style.display === "flex") {
       hidden.style.display = "none";
     } else {
-      hidden.style.display = "block";
+      hidden.style.display = "flex";
     }
   });
 }
